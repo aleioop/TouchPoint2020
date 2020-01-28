b6010108666298baeba910ce84196be93ce259fd
Angus Lei <angusleikh@gmail.com>
2020-01-28 15:24:56 -0800

    Font changes

--- js/main.js
+++ js/main.js
@@ -14,48 +14,44 @@ $(document).ready(function(){
     }
   }
 
-  $.when(initAnimation()).then(initHovers());
-
   function initHovers() {
-    $("#full-circ").hover(function(){
-      $(this).toggleClass("fullhover");
-      $(pacman).toggleClass("fullhover");
-      $(semicirc).toggleClass("fullhover");
-      $(qrtrcirc).toggleClass("fullhover");
-      $("#sub-tickets").toggleClass("hidden");
-    });
+    setTimeout(function(){
+      $("#full-circ").hover(function(){
+        $("#sub-tickets").toggleClass("hidden");
+        $(this).toggleClass("fullhover");
+        $(pacman).toggleClass("fullhover");
+        $(semicirc).toggleClass("fullhover");
+        $(qrtrcirc).toggleClass("fullhover");
+      });
 
-    $("#pacman").hover(function(){
-      $(this).toggleClass("pachover");
-      $(fullcirc).toggleClass("pachover");
-      $(semicirc).toggleClass("pachover");
-      $(qrtrcirc).toggleClass("pachover");
-      $("#sub-interviews").toggleClass("hidden");
-    });
+      $("#pacman").hover(function(){
+        $(this).toggleClass("pachover");
+        $(fullcirc).toggleClass("pachover");
+        $(semicirc).toggleClass("pachover");
+        $(qrtrcirc).toggleClass("pachover");
+        $("#sub-interviews").toggleClass("hidden");
+      });
 
-    $("#semi-circ").hover(function(){
-      $(this).toggleClass("semihover");
-      $(fullcirc).toggleClass("semihover");
-      $(pacman).toggleClass("semihover");
-      $(qrtrcirc).toggleClass("semihover");
-      $("#sub-volunteer").toggleClass("hidden");
-    });
+      $("#semi-circ").hover(function(){
+        $(this).toggleClass("semihover");
+        $(fullcirc).toggleClass("semihover");
+        $(pacman).toggleClass("semihover");
+        $(qrtrcirc).toggleClass("semihover");
+        $("#sub-volunteer").toggleClass("hidden");
+      });
 
-    $("#qrtr-circ").hover(function(){
-      $(this).toggleClass("qrtrhover");
-      $(pacman).toggleClass("qrtrhover");
-      $(semicirc).toggleClass("qrtrhover");
-      $(fullcirc).toggleClass("qrtrhover");
-      $("#sub-info").toggleClass("hidden");
-    });
+      $("#qrtr-circ").hover(function(){
+        $(this).toggleClass("qrtrhover");
+        $(pacman).toggleClass("qrtrhover");
+        $(semicirc).toggleClass("qrtrhover");
+        $(fullcirc).toggleClass("qrtrhover");
+        $("#sub-info").toggleClass("hidden");
+      });
+    }, 1400);
   }
 
+  initAnimation();
+  initHovers();
 
-  // for(let i=0; i<speakersList.length; i++){
-  //   $(speakersList[i]).hover(function(){
-  //     console.log("word");
-  //     $(this).find("subtitle").toggleClass("invisible");
-  //   });
-  // }
 
 });
